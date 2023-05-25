8ed3fcc1effdc0037a75b4d48d805bec47e13a49
Sulaiman1235 <112854352+Sulaiman1235@users.noreply.github.com>
2023-05-25 08:35:50 +0300

    chat gpt
    df

---
+++ pallo peli/scripts.js
@@ -0,0 +1,126 @@
+const canvas = document.getElementById("gameArea");
+const ctx = canvas.getContext("2d");
+
+let x = 200;
+let y = 200;
+let radius = 10;
+let speed = 50;
+
+let upPressed = false;
+let downPressed = false;
+let leftPressed = false;
+let rightPressed = false;
+
+function drawGame() {
+  requestAnimationFrame(drawGame);
+  clearScreen();
+  inputs();
+  boundryCheck();
+  drawGreenBlob();
+}
+
+function boundryCheck() {
+  
+  if (y < radius) {
+    y = radius;
+  }
+  
+  if (y > canvas.height - radius) {
+    y = canvas.height - radius;
+  }
+  
+  if (x < radius) {
+    x = radius;
+  }
+  
+  if (x > canvas.width - radius) {
+    x = canvas.width - radius;
+  }
+}
+
+function inputs() {
+  if (upPressed) {
+    y = y - speed;
+  }
+  if (downPressed) {
+    y = y + speed;
+  }
+  if (leftPressed) {
+    x = x - speed;
+  }
+  if (rightPressed) {
+    x = x + speed;
+  }
+}
+
+function drawGreenBlob() {
+  ctx.fillStyle = "blue";
+  if (upPressed) {
+    ctx.fillStyle = "red";
+  }
+  if (downPressed) {
+    ctx.fillStyle = "red";
+  }
+  if (leftPressed) {
+    ctx.fillStyle = "blue";
+  }
+  if (rightPressed) {
+    ctx.fillStyle = "blue";
+  }
+
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+function clearScreen() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+document.body.addEventListener("keydown", keyDown);
+document.body.addEventListener("keyup", keyUp);
+
+function keyDown(event) {
+  
+  if (event.keyCode == 38) {
+    upPressed = true;
+  }
+
+ 
+  if (event.keyCode == 40) {
+    downPressed = true;
+  }
+ 
+  if (event.keyCode == 37) {
+    leftPressed = true;
+  }
+
+ 
+  if (event.keyCode == 39) {
+    rightPressed = true;
+  }
+}
+
+function keyUp(event) {
+ 
+  if (event.keyCode == 38) {
+    upPressed = false;
+  }
+
+  
+  if (event.keyCode == 40) {
+    downPressed = false;
+  }
+  
+  if (event.keyCode == 37) {
+    leftPressed = false;
+  }
+
+  
+  if (event.keyCode == 39) {
+    rightPressed = false;
+  }
+}
+
+drawGame();
